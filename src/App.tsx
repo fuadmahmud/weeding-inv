5c108b4e0bfd452a52194eae2a2d1d643ec59da0
fuadmahmud <fuadmahmud10@gmail.com>
2025-01-03 14:21:11 +0700

    feat: closing section, lottie animation and new countdown timer

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import './App.css';
 import { Button } from './components/ui/button';
 import { MailOpen, Instagram, MapPin, Gift, CalendarHeart } from 'lucide-react';
@@ -11,10 +11,10 @@ import Height1 from '@/assets/height-1.jpg';
 import Height2 from '@/assets/height-2.jpg';
 import Height3 from '@/assets/height-3.jpg';
 import Height4 from '@/assets/height-4.jpg';
+import Closing from '@/assets/closing.jpg';
 import Card from './components/ui/card';
 import { format } from 'date-fns';
 import { id } from 'date-fns/locale';
-import { DotLottieReact } from '@lottiefiles/dotlottie-react';
 import { Input } from './components/ui/input';
 import { Textarea } from './components/ui/textarea';
 import {
@@ -25,40 +25,20 @@ import {
   SelectTrigger,
   SelectValue
 } from './components/ui/select';
+import FlowerLottie from '@/assets/flower.json';
+import Lottie from 'lottie-react';
+import Timer from './components/ui/timer';
+
+const IMAGE_ROW = [
+  { alt: 'row-1', src: Width1 },
+  { alt: 'row-2', src: Width2 },
+  { alt: 'row-3', src: Width3 }
+];
 
 function App() {
   const [isSliding, setSliding] = useState(false);
-  // const [showBank, setShowBank] = useState(false);
   const targetDate = new Date('2025-03-31');
   const formattedDate = format(targetDate, 'EEEE dd MMMM yyyy', { locale: id });
-  const calculateTimeLeft = () => {
-    const timeDate = targetDate.getTime();
-    const now = new Date().getTime();
-    const difference = timeDate - now;
-
-    if (difference <= 0) {
-      return { days: 0, hours: 0, minutes: 0, seconds: 0 };
-    }
-
-    return {
-      days: Math.floor(difference / (1000 * 60 * 60 * 24)),
-      hours: Math.floor(
-        (difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-      ),
-      minutes: Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60)),
-      seconds: Math.floor((difference % (1000 * 60)) / 1000)
-    };
-  };
-
-  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
-
-  useEffect(() => {
-    const timer = setInterval(() => {
-      setTimeLeft(calculateTimeLeft());
-    }, 1000);
-
-    return () => clearInterval(timer);
-  }, []);
 
   return (
     <>
@@ -110,7 +90,9 @@ function App() {
             </div>
 
             <div className="curved-edge bg-gold h-20 flex flex-col items-center justify-center text-center text-playwrite text-lg text-night">
-              We're Getting Married 💞
+              <span className="animate-in slide-in-from-top hover:running duration-200">
+                We're Getting Married 💞
+              </span>
             </div>
           </div>
         </div>
@@ -165,24 +147,8 @@ function App() {
             Dan Kami bersyukur, dipertemukan Allah diwaktu terbaik, Kini kami
             menanti hari istimewa kami.
           </p>
-          <div className="flex flex-row items-center gap-2 text-gold">
-            <span className="bg-night p-2 rounded w-14 text-sm">
-              {timeLeft.days}
-              <br /> Hari
-            </span>
-            <span className="bg-night p-2 rounded w-14 text-sm">
-              {timeLeft.hours}
-              <br /> Jam
-            </span>
-            <span className="bg-night p-2 rounded w-14 text-sm">
-              {timeLeft.minutes}
-              <br />
-              Menit
-            </span>
-            <span className="bg-night p-2 rounded w-14 text-sm">
-              {timeLeft.seconds}
-              <br /> Detik
-            </span>
+          <div className="w-full">
+            <Timer targetDate="2025-03-31" />
           </div>
         </div>
         {/* Akad & Resepsi */}
@@ -198,7 +164,12 @@ function App() {
                 Lihat lokasi
               </Button>
             </div>
-            <DotLottieReact src="@/assets/flower.json" autoplay loop />
+            <Lottie
+              className="h-48 w-48"
+              animationData={FlowerLottie}
+              loop
+              autoPlay
+            />
             <div className="flex flex-col gap-2 justify-center items-center">
               <p className="text-playwrite text-xl">Resepsi</p>
               <p>{formattedDate}</p>
@@ -218,24 +189,15 @@ function App() {
             Dan segala sesuatu Kami ciptakan berpasang-pasangan agar kamu
             mengingat (kebesaran Allah).
           </p>
-          <p>- QS. Az-Zariyat 49 -</p>
-          <img
-            src={Width1}
-            alt="gallery-img-1"
-            className="h-56 w-full rounded-lg mt-12"
-          />
-          <img
-            src={Width2}
-            alt="gallery-img-2"
-            className="h-56 w-full rounded-lg mt-3"
-            loading="lazy"
-          />
-          <img
-            src={Width3}
-            alt="gallery-img-3"
-            className="h-56 w-full rounded-lg mt-3"
-            loading="lazy"
-          />
+          <p className="mb-9">- QS. Az-Zariyat 49 -</p>
+          {IMAGE_ROW.map(item => (
+            <img
+              src={item.src}
+              key={item.alt}
+              alt={'gallery' + item.alt}
+              className="h-56 w-full rounded-lg mt-3"
+            />
+          ))}
           <div className="flex flex-row items-center justify-between">
             <img
               src={Height1}
@@ -268,14 +230,12 @@ function App() {
         {/* Gift */}
         <div className="flex flex-col items-center justify-center p-4 text-night my-40">
           <p className="text-playwrite text-xl mb-12">Amplop Digital</p>
-          <div className="text-playfair mb-4">
+          <div className="mb-4">
             <p>
               Doa Restu Anda merupakan <br />
-              karunia yang sangat berarti bagi kami.
-              <br />
-              <br />
-              Dan jika memberi adalah ungkapan tanda kasih
-              <br /> Anda, Anda dapat memberi kado secara cashless.
+              karunia yang sangat berarti bagi kami. Dan jika memberi adalah
+              ungkapan tanda kasih Anda, Anda dapat memberi kado secara
+              cashless.
             </p>
           </div>
 
@@ -291,11 +251,12 @@ function App() {
         <div className="bg-gold p-4 py-40 text-night flex flex-col items-center justify-center">
           <p className="text-playwrite xl mb-12">Ucapkan Sesuatu</p>
           <div className="flex flex-row items-center justify-between">
-            <div className="bg-green-300 p-4 font-bold w-[45%] h-24 text-center rounded">
-              10 Hadir
+            <div className="flex items-center justify-center bg-green-300 p-4 font-bold w-[45%] h-24 text-center rounded">
+              10
+              <br /> Hadir
             </div>
-            <div className="bg-red-300 p-4 font-bold w-[45%] h-24 text-center rounded">
-              1 Tidak Hadir
+            <div className="flex items-center justify-center bg-red-300 p-4 font-bold w-[45%] h-24 text-center rounded">
+              1<br /> Tidak Hadir
             </div>
           </div>
           <div className="w-full flex flex-col gap-3 mt-4">
@@ -320,7 +281,18 @@ function App() {
           <Button className="w-full mt-3">Kirim</Button>
         </div>
         {/* Closing */}
-        {/* <div></div> */}
+        <div className="flex flex-col items-center justify-center px-4 py-40 text-night gap-3">
+          <div className="border-gold border-2 rounded-t-[64px] rounded-b-[64px] h-64 w-44 mt-6 overflow-hidden">
+            <img src={Closing} alt="closing image" />
+          </div>
+          <p className="text-sm mt-12">
+            Merupakan suatu kehormatan dan kebahagiaan bagi kami, apabila
+            Bapak/Ibu/Saudara/i berkenan hadir dan memberikan doa restu. Atas
+            kehadiran dan doa restunya, kami mengucapkan terima kasih.
+          </p>
+          <p className="text-playwrite">Wassalamu'alaikum Wr. Wb.</p>
+          <p className="text-playwrite text-xl">Gunawan & Novita</p>
+        </div>
         {/* Footer */}
         <div className="text-night py-10">
           <span>
---
+++ src/components/ui/timer.tsx
@@ -0,0 +1,165 @@
+import React, { useEffect, useRef, useState } from 'react';
+
+interface FlipCardProps {
+  dataAttribute: string;
+  value: number;
+}
+
+interface FlipCardRef extends HTMLDivElement {
+  querySelector(selector: string): HTMLElement | null;
+}
+
+interface TimeLeft {
+  days: number;
+  hours: number;
+  minutes: number;
+  seconds: number;
+}
+
+const FlipCard: React.FC<FlipCardProps> = ({ dataAttribute, value }) => {
+  const cardRef = useRef<FlipCardRef>(null);
+
+  const flip = (newNumber: number) => {
+    if (!cardRef.current) return;
+
+    const topHalf = cardRef.current.querySelector(".top") as HTMLElement;
+    const bottomHalf = cardRef.current.querySelector(".bottom") as HTMLElement;
+    
+    if (!topHalf || !bottomHalf) return;
+    
+    const startNumber = parseInt(topHalf.textContent || '0');
+    if (newNumber === startNumber) return;
+
+    const topFlip = document.createElement("div");
+    const bottomFlip = document.createElement("div");
+    topFlip.classList.add("top-flip");
+    bottomFlip.classList.add("bottom-flip");
+
+    topHalf.textContent = startNumber.toString();
+    bottomHalf.textContent = startNumber.toString();
+    topFlip.textContent = startNumber.toString();
+    bottomFlip.textContent = newNumber.toString();
+
+    topFlip.addEventListener("animationstart", () => {
+      topHalf.textContent = newNumber.toString();
+    });
+
+    topFlip.addEventListener("animationend", () => {
+      topFlip.remove();
+    });
+
+    bottomFlip.addEventListener("animationend", () => {
+      bottomHalf.textContent = newNumber.toString();
+      bottomFlip.remove();
+    });
+
+    cardRef.current.append(topFlip, bottomFlip);
+  };
+
+  useEffect(() => {
+    flip(value);
+  }, [value]);
+
+  return (
+    <div className="flip-card" ref={cardRef} data-attribute={dataAttribute}>
+      <div className="top">0</div>
+      <div className="bottom">0</div>
+    </div>
+  );
+};
+
+const Countdown: React.FC<{ targetDate: string }> = ({ targetDate }) => {
+  const [timeLeft, setTimeLeft] = useState<TimeLeft>({
+    days: 0,
+    hours: 0,
+    minutes: 0,
+    seconds: 0
+  });
+
+  useEffect(() => {
+    const countToDate = new Date(targetDate);
+    countToDate.setHours(countToDate.getHours() + 24);
+
+    const interval = setInterval(() => {
+      const currentDate = new Date();
+      const timeBetweenDates = Math.ceil((countToDate.getTime() - currentDate.getTime()) / 1000);
+
+      const seconds = timeBetweenDates % 60;
+      const minutes = Math.floor(timeBetweenDates / 60) % 60;
+      const hours = Math.floor(timeBetweenDates / 3600) % 24;
+      const days = Math.floor(timeBetweenDates / (3600 * 24));
+
+      setTimeLeft({
+        days,
+        hours,
+        minutes,
+        seconds
+      });
+    }, 250);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div className="countdown-container">
+      <div className="flex flex-col items-center justify-center w-1/5">
+        <div className="days-container">
+          <FlipCard 
+            dataAttribute="data-days-tens" 
+            value={Math.floor(timeLeft.days / 10)} 
+          />
+          <FlipCard 
+            dataAttribute="data-days-ones" 
+            value={timeLeft.days % 10} 
+          />
+        </div>
+        <span className="uppercase text-gray-400">Hari</span>
+      </div>
+
+      <div className="flex flex-col items-center justify-center w-1/5">
+        <div className="hours-container">
+          <FlipCard 
+            dataAttribute="data-hours-tens" 
+            value={Math.floor(timeLeft.hours / 10)} 
+            />
+          <FlipCard 
+            dataAttribute="data-hours-ones" 
+            value={timeLeft.hours % 10} 
+            />
+        </div>
+
+        <span className="uppercase text-gray-400">Jam</span>
+      </div>
+      <div className="flex flex-col items-center justify-center w-1/5">
+        <div className="minutes-container">
+          <FlipCard 
+            dataAttribute="data-minutes-tens" 
+            value={Math.floor(timeLeft.minutes / 10)} 
+          />
+          <FlipCard 
+            dataAttribute="data-minutes-ones" 
+            value={timeLeft.minutes % 10} 
+          />
+        </div>
+
+        <span className="uppercase text-gray-400">Menit</span>
+      </div>
+      <div className="flex flex-col items-center justify-center w-1/5">
+        <div className="seconds-container">
+          <FlipCard 
+            dataAttribute="data-seconds-tens" 
+            value={Math.floor(timeLeft.seconds / 10)} 
+          />
+          <FlipCard 
+            dataAttribute="data-seconds-ones" 
+            value={timeLeft.seconds % 10} 
+          />
+        </div>
+        
+        <span className="uppercase text-gray-400">Detik</span>
+      </div>
+    </div>
+  );
+};
+
+export default Countdown;
